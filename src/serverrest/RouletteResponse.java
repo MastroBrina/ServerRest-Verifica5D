36280e7b08140889f947524671098ecea9ed12a5
Marco Massimi Alunni <marcomassimi08@gmail.com>
2026-02-20 09:29:39 +0100

    fatta classe RouletteResponse
    è stata fatta la classe response con tutti gli attributi di tipo String dato che la risposta te li restituisce stringhe

--- src/serverrest/RouletteResponse.java
+++ src/serverrest/RouletteResponse.java
@@ -9,18 +9,50 @@ package serverrest;
  * @author delfo
  */
 public class RouletteResponse {
-
+    private String giocata;
+    private String numero;
+    private String vittoria;
     
     // Costruttore vuoto necessario per GSON
     public RouletteResponse() {
     }
     
     // Costruttore con parametri
-   
+    public RouletteResponse(String giocata, String numero, String vittoria) {
+        this.giocata = giocata;
+        this.numero = numero;
+        this.vittoria = vittoria;
+    }
     
     // Getter
-   
+   public String getGiocata() {
+        return giocata;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getVittoria() {
+        return vittoria;
+    }
     
     // Setter
+
+    public void setGiocata(String giocata) {
+        this.giocata = giocata;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setVittoria(String vittoria) {
+        this.vittoria = vittoria;
+    }
+    
+    
+
+    
     
 }
